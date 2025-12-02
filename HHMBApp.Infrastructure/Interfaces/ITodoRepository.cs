e530cd79b8c11691a9bc83011346c48f9ca27842
Levente Bajnoczi <bajnoczi.levente@gmail.com>
2025-12-02 19:22:39 +0100

    Add TodoRepository interface

---
+++ HHMBApp.Infrastructure/Interfaces/ITodoRepository.cs
@@ -0,0 +1,18 @@
+﻿using HHMBApp.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HHMBApp.Infrastructure.Interfaces
+{
+    public interface ITodoRepository
+    {
+        Task<Todo> Create(Todo todo);
+        Task<Todo?> Read(Guid id);
+        Task<IEnumerable<Todo>> ReadAll();
+        Task<Todo?> Update(Todo todo);
+        Task<Todo?> Delete(Guid id);
+    }
+}
