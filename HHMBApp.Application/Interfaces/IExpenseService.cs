ad62bf2b6c579a945846a089d232d40b91901987
Levente Bajnoczi <bajnoczi.levente@gmail.com>
2025-12-02 13:35:40 +0100

    Add ExpenseService interface

---
+++ HHMBApp.Application/Interfaces/IExpenseService.cs
@@ -0,0 +1,18 @@
+﻿using HHMBApp.Application.DTOs.Expense;
+using HHMBApp.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HHMBApp.Application.Interfaces
+{
+    public interface IExpenseService
+    {
+        Task<CreateExpenseResponseDto> CreateIncome(CreateExpenseDto request);
+        Task<CreateExpenseResponseDto> UpdateIncome(UpdateExpenseDto request);
+        Task<IEnumerable<Income>> ReadIncomes(Guid householdId);
+        Task<DeleteExpenseResponseDto> DeleteIncome(Guid id, Guid householdId);
+    }
+}
