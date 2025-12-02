4be776f9bf9da818713e281685bdbf6176be9f56
Levente Bajnoczi <bajnoczi.levente@gmail.com>
2025-12-02 19:49:36 +0100

    Add TodoService interface

---
+++ HHMBApp.Application/Interfaces/ITodoService.cs
@@ -0,0 +1,18 @@
+﻿using HHMBApp.Application.DTOs.Todo;
+using HHMBApp.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HHMBApp.Application.Interfaces
+{
+    public interface ITodoService
+    {
+        Task<CreateTodoResponseDto> CreateTodo(CreateTodoDto createTodoDto);
+        Task<IEnumerable<Todo>> ReadAllTodos(Guid householdId);
+        Task<CreateTodoResponseDto> UpdateTodo(UpdateTodoDto updateTodoDto);
+        Task<DeleteTodoResponseDto> DeleteTodo(Guid id, Guid householdId);
+    }
+}
