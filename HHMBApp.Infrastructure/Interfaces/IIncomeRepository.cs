d5fe03a2dd931cec723f3a2aa1db1fc6c9e8bf05
Levente Bajnoczi <bajnoczi.levente@gmail.com>
2025-12-02 10:25:41 +0100

    Added IncomeRepository interface

---
+++ HHMBApp.Infrastructure/Interfaces/IIncomeRepository.cs
@@ -0,0 +1,18 @@
+﻿using HHMBApp.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HHMBApp.Infrastructure.Interfaces
+{
+    public interface IIncomeRepository
+    {
+        Task<Income?> Create(Income income);
+        Task<Income?> Read(Guid id);
+        Task<IEnumerable<Income>> ReadAll();
+        Task<Income?> Update(Income income);
+        Task<Income?> Delete(Guid id);
+    }
+}
