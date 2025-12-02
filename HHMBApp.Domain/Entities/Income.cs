4a9c4a960bc6f0ae41fe4dc3ee7456c4302d154f
Levente Bajnoczi <bajnoczi.levente@gmail.com>
2025-12-02 11:32:09 +0100

    Modified Income model

--- HHMBApp.Domain/Entities/Income.cs
+++ HHMBApp.Domain/Entities/Income.cs
@@ -11,4 +11,6 @@ public class Income
     public int Amount { get; set; }
     public DateTime Date { get; set; }
     public Category Category { get; set; } = null!;
+    public string Title { get; set; } = null!;
+    public string Description { get; set; } = null!;
 }
