3244a1a4b47bf93a0457f8a855076ba84e405b21
Levente Bajnoczi <bajnoczi.levente@gmail.com>
2025-12-02 11:32:29 +0100

    Added IncomeService interface

---
+++ HHMBApp.Application/Interfaces/IIncomeService.cs
@@ -0,0 +1,18 @@
+﻿using HHMBApp.Application.DTOs;
+using HHMBApp.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HHMBApp.Application.Interfaces
+{
+    public interface IIncomeService
+    {
+        Task<CreateIncomeResponseDto> CreateIncome(CreateIncomeDto request);
+        Task<CreateIncomeResponseDto> UpdateIncome(UpdateIncomeDto request);
+        Task<IEnumerable<Income>> ReadIncomes(Guid householdId);
+        Task<DeleteIncomeResponseDto> DeleteIncome(Guid id, Guid householdId);
+    }
+}
