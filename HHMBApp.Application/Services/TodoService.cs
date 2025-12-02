927bc2bacde5c9fbb27e4d992686b5553e3d4443
Levente Bajnoczi <bajnoczi.levente@gmail.com>
2025-12-02 19:50:45 +0100

    Add TodoService

---
+++ HHMBApp.Application/Services/TodoService.cs
@@ -0,0 +1,42 @@
+﻿using HHMBApp.Application.DTOs.Todo;
+using HHMBApp.Application.Interfaces;
+using HHMBApp.Domain.Entities;
+using HHMBApp.Infrastructure.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HHMBApp.Application.Services
+{
+    public class TodoService : ITodoService
+    {
+        private readonly ITodoRepository _todoRepository;
+
+        public TodoService(ITodoRepository todoRepository)
+        {
+            _todoRepository = todoRepository;
+        }
+
+        public Task<CreateTodoResponseDto> CreateTodo(CreateTodoDto createTodoDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<DeleteTodoResponseDto> DeleteTodo(Guid id, Guid householdId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Todo>> ReadAllTodos(Guid householdId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<CreateTodoResponseDto> UpdateTodo(UpdateTodoDto updateTodoDto)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
