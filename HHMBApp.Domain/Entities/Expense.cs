6526d525aa36c9f5b5bc561bb0d7ba5edea7501a
Levente Bajnoczi <bajnoczi.levente@gmail.com>
2025-12-02 13:17:57 +0100

    Add Expense model

---
+++ HHMBApp.Domain/Entities/Expense.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HHMBApp.Domain.Entities
+{
+    public class Expense
+    {
+        [Key]
+        public Guid Id { get; set; }
+        public Guid UserId { get; set; }
+        public Guid HouseholdId { get; set; }
+        public int Amount { get; set; }
+        public DateTime Date { get; set; }
+        public Guid CategoryId { get; set; }
+        public string Title { get; set; } = null!;
+        public string Description { get; set; } = null!;
+        public string? ReceiptBase64 { get; set; }
+    }
+}
