cc2be3cff1e5d0243b43b9c3802e7661e1b9082f
Levente Bajnoczi <bajnoczi.levente@gmail.com>
2025-12-02 13:20:33 +0100

    Add ExpenseRepository interface

---
+++ HHMBApp.Infrastructure/Interfaces/IExpenseRepository.cs
@@ -0,0 +1,18 @@
+﻿using HHMBApp.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HHMBApp.Infrastructure.Interfaces
+{
+    public interface IExpenseRepository
+    {
+        Task<Expense?> Create(Expense expense);
+        Task<Expense?> Read(Guid id);
+        Task<IEnumerable<Expense>> ReadAll();
+        Task<Expense?> Update(Expense expense);
+        Task<Expense?> Delete(Guid id);
+    }
+}
