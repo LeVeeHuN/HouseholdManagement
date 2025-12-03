9c15d50759a7ab54d9b22ef1b67deb4dabbdff88
Levente Bajnoczi <bajnoczi.levente@gmail.com>
2025-12-03 11:43:54 +0100

    Add Wish model

---
+++ HHMBApp.Domain/Entities/Wish.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HHMBApp.Domain.Entities
+{
+    public class Wish
+    {
+        [Key]
+        public Guid Id { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime ClosedAt { get; set; }
+        public Guid CreatedByUserId { get; set; }
+        public Guid ClosedByUserId { get; set; }
+        public int ApproxAmount { get; set; }
+        public Guid HouseholdId { get; set; }
+        public string Title { get; set; } = null!;
+        public string? Description { get; set; }
+    }
+}
