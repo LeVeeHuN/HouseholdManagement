9bdb817c94e9f7136a568c647e4b2354a962d2d3
Levente Bajnoczi <bajnoczi.levente@gmail.com>
2025-12-03 22:07:16 +0100

    Add HouseholdRepository interface and implemented HouseholdRepository

---
+++ HHMBApp.Infrastructure/Interfaces/IHouseholdRepository.cs
@@ -0,0 +1,18 @@
+﻿using HHMBApp.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HHMBApp.Infrastructure.Interfaces
+{
+    public interface IHouseholdRepository
+    {
+        Task<Household> Create(Household household);
+        Task<Household?> Read(Guid id);
+        Task<IEnumerable<Household>> ReadAll();
+        Task<Household?> Update(Household household);
+        Task<Household?> Delete(Guid id);
+    }
+}
---
+++ HHMBApp.Infrastructure/Repositories/HouseholdRepository.cs
@@ -0,0 +1,64 @@
+﻿using HHMBApp.Domain.Entities;
+using HHMBApp.Infrastructure.Data;
+using HHMBApp.Infrastructure.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HHMBApp.Infrastructure.Repositories
+{
+    public class HouseholdRepository : IHouseholdRepository
+    {
+        private readonly AppDbContext _context;
+
+        public HouseholdRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Household> Create(Household household)
+        {
+            var result = await _context.Households.AddAsync(household);
+            await _context.SaveChangesAsync();
+            return result.Entity;
+        }
+
+        public async Task<Household?> Delete(Guid id)
+        {
+            var result = await _context.Households.FindAsync(id);
+            if (result == null)
+            {
+                return null;
+            }
+            _context.Households.Remove(result);
+            await _context.SaveChangesAsync();
+            return result;
+        }
+
+        public async Task<Household?> Read(Guid id)
+        {
+            return await _context.Households.FindAsync(id);
+        }
+
+        public async Task<IEnumerable<Household>> ReadAll()
+        {
+            return await _context.Households.ToListAsync();
+        }
+
+        public async Task<Household?> Update(Household household)
+        {
+            var result = await _context.Households.FindAsync(household.Id);
+            if (result == null)
+            {
+                return null;
+            }
+
+            _context.Households.Update(household);
+            await _context.SaveChangesAsync();
+            return household;
+        }
+    }
+}
