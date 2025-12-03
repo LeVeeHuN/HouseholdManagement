55b8581a352a3f3527b572405ec2179171647290
Levente Bajnoczi <bajnoczi.levente@gmail.com>
2025-12-03 23:34:28 +0100

    Add HouseholdService interface and related DTOs

---
+++ HHMBApp.Application/DTOs/Household/UpdateHouseholdResponseDto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HHMBApp.Application.DTOs.Household
+{
+    public class UpdateHouseholdResponseDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        public string JoinCode { get; set; } = null!;
+        public UpdateHouseholdStatus Response { get; set; }
+    }
+
+    public enum UpdateHouseholdStatus
+    {
+        OK = 0,
+        UpdateHouseholdError = 1,
+    }
+}
---
+++ HHMBApp.Application/Interfaces/IHouseholdService.cs
@@ -0,0 +1,17 @@
+﻿using HHMBApp.Application.DTOs.Household;
+using HHMBApp.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HHMBApp.Application.Interfaces
+{
+    public interface IHouseholdService
+    {
+        Task<Household> CreateHousehold(string name);
+        Task<Household?> GetHouseholdById(Guid id);
+        Task<UpdateHouseholdResponseDto> ChangeHouseholdName(Guid householdId, string newName);
+    }
+}
