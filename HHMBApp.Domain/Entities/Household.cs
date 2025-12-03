377f4e671432f964f04c4591e2f33f936fd5712d
Levente Bajnoczi <bajnoczi.levente@gmail.com>
2025-12-03 21:12:17 +0100

    Add Household model

---
+++ HHMBApp.Domain/Entities/Household.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HHMBApp.Domain.Entities
+{
+    public class Household
+    {
+        [Key]
+        public Guid Id { get; set; }
+        public string Name { get; set; } = null!;
+        public string JoinCode { get; set; } = null!;
+    }
+}
