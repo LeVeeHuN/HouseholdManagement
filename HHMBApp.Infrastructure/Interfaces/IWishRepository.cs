1a2c5879e5fcbbeaf0cfb464e69db8ab988e8c56
Levente Bajnoczi <bajnoczi.levente@gmail.com>
2025-12-03 11:44:26 +0100

    Add WishRepository interface

---
+++ HHMBApp.Infrastructure/Interfaces/IWishRepository.cs
@@ -0,0 +1,18 @@
+﻿using HHMBApp.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HHMBApp.Infrastructure.Interfaces
+{
+    public interface IWishRepository
+    {
+        Task<Wish> Create(Wish wish);
+        Task<Wish?> Read(Guid id);
+        Task<IEnumerable<Wish>> ReadAll();
+        Task<Wish?> Update(Wish wish);
+        Task<Wish?> Delete(Guid id);
+    }
+}
