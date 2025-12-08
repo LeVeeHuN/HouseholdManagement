8c42aa269e1578d49bf74db2d8cb956fece85437
Levente Bajnoczi <bajnoczi.levente@gmail.com>
2025-12-08 14:10:24 +0100

    Add AuthController with register and login endpoints, also updated program.cs, added missing DI elements, and fixed other bugs and made improvements

---
+++ HHMBApp.API/Controllers/AuthController.cs
@@ -0,0 +1,44 @@
+﻿using HHMBApp.Application.DTOs.User;
+using HHMBApp.Application.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HHMBApp.API.Controllers
+{
+    [ApiController]
+    [AllowAnonymous]
+    public class AuthController : ControllerBase
+    {
+        private readonly IUserService _userService;
+
+        public AuthController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        [HttpPost("api/auth/register")]
+        public async Task<ActionResult<CreateUserResponseDto>> Register(CreateUserDto userDto)
+        {
+            var result = await _userService.AddUser(userDto);
+            if (result.Result == CreateUserResult.OK)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+
+        [HttpPost("api/auth/login")]
+        public async Task<ActionResult<LoginResponseDto>> Login(LoginRequestDto loginRequest)
+        {
+            var result = await _userService.Login(loginRequest);
+            if (result.Result == LoginResponseStatus.OK)
+            {
+                return Ok(result);
+            }
+            return Unauthorized(result);
+        }
+
+        // TODO: Add change password
+    }
+}
--- HHMBApp.API/Program.cs
+++ HHMBApp.API/Program.cs
@@ -1,3 +1,14 @@
+using HHMBApp.Application.Interfaces;
+using HHMBApp.Application.Services;
+using HHMBApp.Application.Settings;
+using HHMBApp.Infrastructure.Data;
+using HHMBApp.Infrastructure.Interfaces;
+using HHMBApp.Infrastructure.Repositories;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
 namespace HHMBApp.API
 {
     public class Program
@@ -5,7 +16,59 @@ namespace HHMBApp.API
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+            builder.Services.Configure<JwtOptions>(builder.Configuration.GetSection(JwtOptions.SectionName));
+
+            builder.Services.AddSingleton<IJwtTokenService, JwtTokenService>();
+
+            var jwtSection = builder.Configuration.GetSection(JwtOptions.SectionName);
+            var jwtOptions = jwtSection.Get<JwtOptions>();
+
+            builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options =>
+                {
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuer = true,
+                        ValidateAudience = true,
+                        ValidateLifetime = true,
+                        ValidateIssuerSigningKey = true,
+                        ValidIssuer = jwtOptions.Issuer,
+                        ValidAudience = jwtOptions.Audience,
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtOptions.Key))
+                    };
+                });
+
+            builder.Services.AddAuthorization();
+            builder.Services.AddControllers();
+
+            // Register services
+            builder.Services.AddDbContext<AppDbContext>(options => 
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+            builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
+            builder.Services.AddScoped<IExpenseRepository, ExpenseRepository>();
+            builder.Services.AddScoped<IGroceryListItemRepository, GroceryListItemRepository>();
+            builder.Services.AddScoped<IGroceryListRepository, GroceryListRepository>();
+            builder.Services.AddScoped<IHouseholdRepository, HouseholdRepository>();
+            builder.Services.AddScoped<IIncomeRepository, IncomeRepository>();
+            builder.Services.AddScoped<ITodoRepository, TodoRepository>();
+            builder.Services.AddScoped<IUserRepository, UserRepository>();
+            builder.Services.AddScoped<IWishRepository, WIshRepository>();
+
+            builder.Services.AddScoped<ICategoryService, CategoryService>();
+            builder.Services.AddScoped<IExpenseService, ExpenseService>();
+            builder.Services.AddScoped<IGroceryService, GroceryService>();
+            builder.Services.AddScoped<IHouseholdService, HouseholdService>();
+            builder.Services.AddScoped<IIncomeService, IncomeService>();
+            builder.Services.AddScoped<ITodoService, TodoService>();
+            builder.Services.AddScoped<IUserService, UserService>();
+
             var app = builder.Build();
+
+            app.UseHttpsRedirection();
+            app.UseAuthentication();
+            app.UseAuthorization();
+            app.MapControllers();
 
             app.MapGet("/", () => "Hello World!");
 
