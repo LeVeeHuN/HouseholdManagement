4b1dc758bee8ffd6cbb7a783349680a36a28295b
Levente Bajnoczi <bajnoczi.levente@gmail.com>
2025-12-09 19:01:15 +0100

    Add Delete expense and income DTOs

---
+++ HHMBApp.Application/DTOs/Expense/DeleteExpenseDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HHMBApp.Application.DTOs.Expense
+{
+    public class DeleteExpenseDto
+    {
+        public Guid ExpenseId { get; set; }
+        public Guid HouseholdId { get; set; }
+    }
+}
---
+++ HHMBApp.Application/DTOs/Income/DeleteIncomeDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HHMBApp.Application.DTOs.Income
+{
+    public class DeleteIncomeDto
+    {
+        public Guid IncomeId { get; set; }
+        public Guid HouseholdId { get; set; }
+    }
+}
